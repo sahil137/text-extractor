153996b9bda5f84e15f9ae070ad717df167d60d7
sahil <sahilsaini137@gmailcom>
2024-01-16 23:40:06 +0530

    add navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-center items-center mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
+      <h2 className="font-bold text-2xl">Text Extractor</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
